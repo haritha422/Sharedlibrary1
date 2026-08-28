582c3426d162e3f7a3d4b1bc9f6cee4b06e03a12
haritha422 <harithamanjula2002@gmail.com>
2026-08-28 17:31:27 +0530

    Update haritha.groovy

--- vars/haritha.groovy
+++ vars/haritha.groovy
@@ -8,9 +8,9 @@ def build()
 }
 def deploy(jobname,ip,context)
 {
-        sh "scp /home/ubuntu/haritha/workspace/${jobname}/webapp/target/webapp.war ubuntu@${ip}:/var/lib/tomcat10/webapps/${context}.war"
+        sh "scp /var/lib/jenkins/workspace/sharedlibraryscripted/${jobname}/webapp/target/webapp.war ubuntu@${ip}:/var/lib/tomcat10/webapps/${context}.war"
 }
 def testing()
 {
-         sh "java -jar /home/ubuntu/haritha/workspace/scriptedpipelineonslave/testing.jar"
+         sh "java -jar /var/lib/jenkins/workspace/sharedlibraryscripted/testing.jar"
 }
