3267061bfa6c7edeea18449d0c5f8f3cea6e0dfd
haritha422 <harithamanjula2002@gmail.com>
2026-08-28 17:22:45 +0530

    Replace single quotes with double quotes in haritha.groovy

--- vars/haritha.groovy
+++ vars/haritha.groovy
@@ -1,16 +1,16 @@
 def download(repo)
 {
-        git 'https://github.com/IntelliqDevops/${repo}.git'
+        git "https://github.com/IntelliqDevops/${repo}.git"
 }
 def build()
 {
-        sh 'mvn package'
+        sh "mvn package'
 }
 def deploy(jobname,ip,context)
 {
-        sh 'scp /home/ubuntu/haritha/workspace/${jobname}/webapp/target/webapp.war ubuntu@${ip}:/var/lib/tomcat10/webapps/${context}.war'
+        sh "scp /home/ubuntu/haritha/workspace/${jobname}/webapp/target/webapp.war ubuntu@${ip}:/var/lib/tomcat10/webapps/${context}.war"
 }
 def testing()
 {
-         sh 'java -jar /home/ubuntu/haritha/workspace/scriptedpipelineonslave/testing.jar'
+         sh "java -jar /home/ubuntu/haritha/workspace/scriptedpipelineonslave/testing.jar"
 }
